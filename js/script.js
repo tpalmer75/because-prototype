6672cee3581c134f3e304e3a976a8942b0f992dd
tpalmer75 <taylor.hatch.palmer@gmail.com>
2014-11-19 14:11:45 -0700

    New iframe set up, way finding

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-
-// Detect touch screens and add class to HTML
+	// Detect touch screens and add class to HTML
     var is_touch_device = 'ontouchstart' in document.documentElement;
 
     if (is_touch_device) {
@@ -10,9 +9,41 @@ $(document).ready(function() {
     	$('html').addClass('no-touch');
     }
 
-//Scroll detection (doesn't work)
-on.scroll(function() {
-	alert('scroll');
-});
+	// Scroll detection
+	var lastScrollTop = 0;
+	var bottomBar = $('.bottom-bar');
+
+	$(window).scroll(function(event){
+	   var st = $(this).scrollTop();
+	   if (st > lastScrollTop && st > 10){
+	       // down scroll code
+	       bottomBar.addClass('hide');
+	   } else {
+	      // up scroll code
+	      bottomBar.removeClass('hide');
+	   }
+	   lastScrollTop = st;
+	});
+
+	// Filter toggle
+
+	var filterButton = $('.filter-button');
+	var filterContainer = $('#filter')
+
+	filterButton.click(function(e) {
+		filterContainer.toggleClass('show');
+		e.preventDefault();
+	});
+
+	// Bottom bar wayfinder movement
+
+	var bottomTrigger = $('.bottom-bar .section')
+
+	bottomTrigger.click(function() {
+		var placement = $(this).data('left');
+		var wayfinder = $('.wayfinder');
+
+		wayfinder.css('left', placement);
+	});
 
 });
